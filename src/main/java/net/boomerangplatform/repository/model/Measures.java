1f01ed2894106f51fc113f42b1c7c09a6b3e8c92
Glen Hickman <gchickma@us.ibm.com>
2019-06-10 17:50:47 -0500

    add unit_tests fields

--- src/main/java/net/boomerangplatform/repository/model/Measures.java
+++ src/main/java/net/boomerangplatform/repository/model/Measures.java
@@ -1,45 +1,158 @@
 package net.boomerangplatform.repository.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Measures {
 
-  @JsonProperty("ncloc")
-  private Integer ncloc;
+	@JsonProperty("ncloc")
+	private Integer ncloc;
 
-  @JsonProperty("complexity")
-  private Integer complexity;
+	@JsonProperty("complexity")
+	private Integer complexity;
 
-  @JsonProperty("violations")
-  private Integer violations;
+	@JsonProperty("violations")
+	private Integer violations;
 
-  public Measures() {
-    // Do nothing
-  }
+	@JsonProperty("tests")
+	private Integer tests;
 
-  public Integer getNcloc() {
-    return ncloc;
-  }
+	@JsonProperty("test_errors")
+	private Integer testErrors;
 
-  public void setNcloc(Integer ncloc) {
-    this.ncloc = ncloc;
-  }
+	@JsonProperty("test_failures")
+	private Integer testFailures;
 
-  public Integer getComplexity() {
-    return complexity;
-  }
+	@JsonProperty("skipped_tests")
+	private Integer skippedTests;
 
-  public void setComplexity(Integer complexity) {
-    this.complexity = complexity;
-  }
+	@JsonProperty("test_success_density")
+	private Double testSuccessDensity;
 
-  public Integer getViolations() {
-    return violations;
-  }
+	@JsonProperty("test_execution_time")
+	private Integer testExecutionTime;
 
-  public void setViolations(Integer violations) {
-    this.violations = violations;
-  }
+	@JsonProperty("coverage")
+	private Double coverage;
+
+	@JsonProperty("lines_to_cover")
+	private Integer linesToCover;
+
+	@JsonProperty("uncovered_lines")
+	private Integer uncoveredLines;
+
+	@JsonProperty("line_coverage")
+	private Double lineCoverage;
+
+	public Measures() {
+		// Do nothing
+	}
+
+	public Integer getNcloc() {
+		return ncloc;
+	}
+
+	public void setNcloc(Integer ncloc) {
+		this.ncloc = ncloc;
+	}
+
+	public Integer getComplexity() {
+		return complexity;
+	}
+
+	public void setComplexity(Integer complexity) {
+		this.complexity = complexity;
+	}
+
+	public Integer getViolations() {
+		return violations;
+	}
+
+	public void setViolations(Integer violations) {
+		this.violations = violations;
+	}
+
+	public Integer getTests() {
+		return tests;
+	}
+
+	public void setTests(Integer tests) {
+		this.tests = tests;
+	}
+
+	public Integer getTestErrors() {
+		return testErrors;
+	}
+
+	public void setTestErrors(Integer testErrors) {
+		this.testErrors = testErrors;
+	}
+
+	public Integer getTestFailures() {
+		return testFailures;
+	}
+
+	public void setTestFailures(Integer testFailures) {
+		this.testFailures = testFailures;
+	}
+
+	public Integer getSkippedTests() {
+		return skippedTests;
+	}
+
+	public void setSkippedTests(Integer skippedTests) {
+		this.skippedTests = skippedTests;
+	}
+
+	public Double getTestSuccessDensity() {
+		return testSuccessDensity;
+	}
+
+	public void setTestSuccessDensity(Double testSuccessDensity) {
+		this.testSuccessDensity = testSuccessDensity;
+	}
+
+	public Integer getTestExecutionTime() {
+		return testExecutionTime;
+	}
+
+	public void setTestExecutionTime(Integer testExecutionTime) {
+		this.testExecutionTime = testExecutionTime;
+	}
+
+	public Double getCoverage() {
+		return coverage;
+	}
+
+	public void setCoverage(Double coverage) {
+		this.coverage = coverage;
+	}
+
+	public Integer getLinesToCover() {
+		return linesToCover;
+	}
+
+	public void setLinesToCover(Integer linesToCover) {
+		this.linesToCover = linesToCover;
+	}
+
+	public Integer getUncoveredLines() {
+		return uncoveredLines;
+	}
+
+	public void setUncoveredLines(Integer uncoveredLines) {
+		this.uncoveredLines = uncoveredLines;
+	}
+
+	public Double getLineCoverage() {
+		return lineCoverage;
+	}
+
+	public void setLineCoverage(Double lineCoverage) {
+		this.lineCoverage = lineCoverage;
+	}
 }
--- src/main/java/net/boomerangplatform/repository/model/SonarQubeReport.java
+++ src/main/java/net/boomerangplatform/repository/model/SonarQubeReport.java
@@ -1,8 +1,11 @@
 package net.boomerangplatform.repository.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SonarQubeReport {
 
