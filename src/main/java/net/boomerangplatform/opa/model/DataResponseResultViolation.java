f7f201e6fe67b101d70b2c32d1393a534b126a0a
Tyson Lawrie <twlawrie@us.ibm.com>
2019-09-10 13:25:12 +1000

    Minor format update

--- src/main/java/net/boomerangplatform/opa/model/DataResponseResultViolation.java
+++ src/main/java/net/boomerangplatform/opa/model/DataResponseResultViolation.java
@@ -13,22 +13,27 @@ public class DataResponseResultViolation {
   @JsonProperty("valid")
   private Boolean valid;
 
-public String getMetric() {
-return metric;}
+  public String getMetric() {
+    return metric;
+  }
 
-public void setMetric(String metric) {
-this.metric = metric;}
+  public void setMetric(String metric) {
+    this.metric = metric;
+  }
 
-public String getMessage() {
-return message;}
+  public String getMessage() {
+    return message;
+  }
 
-public void setMessage(String message) {
-this.message = message;}
+  public void setMessage(String message) {
+    this.message = message;
+  }
 
-public Boolean getValid() {
-return valid;}
+  public Boolean getValid() {
+    return valid;
+  }
 
-public void setValid(Boolean valid) {
-this.valid = valid;}
-
+  public void setValid(Boolean valid) {
+    this.valid = valid;
+  }
 }
