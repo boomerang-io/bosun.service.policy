b4e33295688954dfa79753e4f136545961b988cd
Glen Hickman <gchickma@us.ibm.com>
2025-05-14 15:43:39 -0500

    add security config

---
+++ src/main/java/net/boomerangplatform/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package net.boomerangplatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
