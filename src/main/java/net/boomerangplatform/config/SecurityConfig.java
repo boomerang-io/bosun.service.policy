6958a156d3f8eb0b70a54a4f0b0cbc09dfcbc5e0
Glen Hickman <gchickma@us.ibm.com>
2025-05-14 16:34:27 -0500

    try anonymous

--- src/main/java/net/boomerangplatform/config/SecurityConfig.java
+++ src/main/java/net/boomerangplatform/config/SecurityConfig.java
@@ -2,20 +2,37 @@ package net.boomerangplatform.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, proxyTargetClass = true)
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().permitAll()
-            );
-        return http.build();
-    }
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll().anyRequest().authenticated())
+//				.exceptionHandling(
+//						exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+//							response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+//						}))
+//				.httpBasic(Customizer.withDefaults());
+//		return http.build();
+//	}
+
+	@Bean
+	public SecurityFilterChain setupNone(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.disable()).anonymous(a -> a.authorities()).build();
+	}
 }
