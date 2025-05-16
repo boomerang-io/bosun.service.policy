d5f70f65454e2d64c1083ec69c72d2973db5b688
Glen Hickman <gchickma@us.ibm.com>
2025-05-16 14:33:18 -0500

    add @EnableMongoRepositories annotation

--- src/main/java/net/boomerangplatform/Application.java
+++ src/main/java/net/boomerangplatform/Application.java
@@ -3,11 +3,13 @@ package net.boomerangplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.client.RestTemplate;
 
 import io.swagger.v3.oas.models.OpenAPI;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class Application {
 
   public static void main(String[] args) {
